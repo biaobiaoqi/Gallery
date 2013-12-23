e8642e496fd0f5b0d64544cf81f9d6eaf60ec222
biaobiaoqi <shenyapeng@gmail.com>
2013-12-24 03:52:18 +0800

    little chagnes

--- js/demo.js
+++ js/demo.js
@@ -10,7 +10,7 @@
 $(function () {
     'use strict';
 
-    // Load demo images from api:
+    // Load demo images from api: getHorizPhotosList
     $.ajax({
         url: (window.location.protocol === 'https:' ?
                 'https://' : 'http://') +
@@ -23,7 +23,44 @@ $(function () {
         jsonp: 'callback'
     }).done(function (result) {
         var carouselLinks = [],
-            linksContainer = $('#links'),
+            linksContainer = $('#hlinks'),
+            baseUrl;
+        // Add the demo images as links with thumbnails to the page:
+        $.each(result.photos, function (index, photo) {
+            baseUrl = 'http://r-gallery.u.qiniudn.com/' + photo.id; 
+            $('<a/>')
+                .append($('<img>').prop('src', baseUrl + '?imageView/2/w/75/h/75')) //small
+                .prop('href', baseUrl + '?imageView/2/w/800/h/800') //big
+                .prop('title', photo.title)
+                .attr('data-gallery', '')
+                .appendTo(linksContainer);
+            carouselLinks.push({
+                href: baseUrl + '?imageView/2/w/600/h/600',//middle
+                title: photo.title
+            });
+        });
+        // Initialize the Gallery as image carousel:
+        blueimp.Gallery(carouselLinks, {
+            container: '#blueimp-image-carousel',
+            carousel: true
+        });
+    });
+
+
+    // Load demo images from api: getVerticPhotosList
+    $.ajax({
+        url: (window.location.protocol === 'https:' ?
+                'https://' : 'http://') +
+                '106.186.114.43:9292/getVerticPhotosList/',
+        data: {
+            format: 'json',
+            api_key: '7617adae70159d09ba78cfec73c13be3' // jshint ignore:line
+        },
+        dataType: 'jsonp',
+        jsonp: 'callback'
+    }).done(function (result) {
+        var carouselLinks = [],
+            linksContainer = $('#vlinks'),
             baseUrl;
         // Add the demo images as links with thumbnails to the page:
         $.each(result.photos, function (index, photo) {
